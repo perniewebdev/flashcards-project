a2d2d135a36243a6fd8da786aee93d6416f986b4
perniewebdev <183206536+perniewebdev@users.noreply.github.com>
2026-03-01 00:47:07 +0100

    Add database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const cors = require("cors");
 
@@ -6,8 +5,10 @@ const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
 const deckRoutes = require("./routes/decks");
 
+const pool = require("./database");
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +16,21 @@ app.use(express.json());
 app.use("/users", userRoutes);
 app.use("/auth", authRoutes);
 app.use("/decks", deckRoutes);
+
+async function initDB() {
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS users (
+      id UUID PRIMARY KEY,
+      email TEXT UNIQUE NOT NULL,
+      password TEXT NOT NULL,
+      accept_tos BOOLEAN NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+  `);
+  console.log("Database ready");
+}
+
+initDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
